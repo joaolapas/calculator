8a8a0ac70075f32c4cadfa240fac595b42edffd6
Joao Lapas <artesanatoavela@gmail.com>
2022-12-30 12:49:00 +0000

    fixing a bug

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://github.com/joaolapas/calculator'
+  base: 'https://github.com/joaolapas/calculator',
   plugins: [react()],
 })
